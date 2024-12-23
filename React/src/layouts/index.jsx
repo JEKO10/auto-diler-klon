1c4378679c7150972a4996b11e6f134a358187f6
Aleksa <aleksa3b@gmail.com>
2024-12-23 14:40:04 +0100

    hero background image fix

--- React/src/layouts/index.jsx
+++ React/src/layouts/index.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
 export const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
-      <Navbar />
+      {location.pathname === "/" ? "" : <Navbar />}
       <Outlet />
       <Footer />
     </>
--- React/src/pages/home/index.jsx
+++ React/src/pages/home/index.jsx
@@ -3,10 +3,12 @@ import HeroSection from "./components/HeroSection";
 import AutoShop from "./components/AutoShop";
 import PlatformNumbers from "./components/PlatformNumbers";
 import CarListing from "./components/CarListing";
+import Navbar from "../../components/Navbar";
 
 const Home = () => {
   return (
     <div className="home">
+      <Navbar />
       <HeroSection />
       <FilterForm />
       <CarListing />
